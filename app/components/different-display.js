6c9abe9a82a9f3a8998808761b5ca411bb06566d
Colleen <student@epicodus.com>
2015-10-06 16:08:31 -0700

    added gallery toggle in different-display component and links to title

--- app/components/different-display.js
+++ app/components/different-display.js
@@ -3,13 +3,21 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   showList: true,
   showThumbnail: false,
+  showGallery: false,
   actions:{
     thumbnailFormShow() {
+      this.set('showGallery', false),
       this.set('showList', false),
       this.set('showThumbnail', true);
     },
     listFormShow() {
+      this.set('showGallery', false),
       this.set('showList', true),
+      this.set('showThumbnail', false);
+    },
+    galleryFormShow() {
+      this.set('showGallery', true),
+      this.set('showList', false),
       this.set('showThumbnail', false);
     }
   }
