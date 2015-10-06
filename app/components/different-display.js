4989a764868f86e29972d8b46e5dec6bd4c91f3e
Colleen <student@epicodus.com>
2015-10-06 16:00:04 -0700

    successful toggle between thumbnail and list

--- app/components/different-display.js
+++ app/components/different-display.js
@@ -1,10 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showList: true,
   showThumbnail: false,
   actions:{
     thumbnailFormShow() {
+      this.set('showList', false),
       this.set('showThumbnail', true);
+    },
+    listFormShow() {
+      this.set('showList', true),
+      this.set('showThumbnail', false);
     }
   }
 });
