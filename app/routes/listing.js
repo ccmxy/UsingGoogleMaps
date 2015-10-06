314e6d9169a6e2f7d3f97377a3ec458c833f75d9
Colleen <student@epicodus.com>
2015-10-06 12:33:54 -0700

    added element to main index from listing.js

--- app/routes/listing.js
+++ app/routes/listing.js
@@ -4,7 +4,10 @@ export default Ember.Route.extend({
   //apparently this function doesn't need to be called "model"
   // but still must be referred to that way
   model(params) {
+    window.parent.$("ul#nav navbar-nav").append("<li>" + "pizza.toppings[i]" + "</li>");
+    var theListing = this.store.findRecord('listing', params.listing_id);
+    window.parent.$(".goesHere").text("test");
+
     return this.store.findRecord('listing', params.listing_id);
   }
-
 });
