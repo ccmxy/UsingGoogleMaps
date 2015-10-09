69a7670cab97f731cf5ded37640e19481e25a930
ccmxy <windrainicefire@gmail.com>
2015-10-08 19:16:55 -0700

    added map marker

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -3,12 +3,35 @@ export default Ember.Component.extend({
 
   actions: {
     showMap(model) {
+    //  var bangalore = { lat: 51.503454, lng: -0.119562 };
+
       var container = this.$('.map-display')[0];
       var options = {
         center: this.get('map').center(model.get('latitude'), model.get('longitude')),
         zoom: 15
       };
-      this.get('map').findMap(container, options);
+      //this.get('map').findMap(container, options);
+
+      //NEW! (With above line for no marker)
+      var myLatlng = new google.maps.LatLng(model.get('latitude'),model.get('longitude'));
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        title: model.get('title')
+      });
+      var theMap = this.get('map').findMap(container, options);
+      marker.setMap(theMap);
+
     }
   }
 });
+/*
+function addMarker(location, map) {
+  // Add the marker at the clicked location, and add the next-available label
+  // from the array of alphabetical characters.
+  var marker = new google.maps.Marker({
+    position: location,
+    label: labels[labelIndex++ % labels.length],
+    map: map  //IMPORTNAT????
+  });
+}
+*/
